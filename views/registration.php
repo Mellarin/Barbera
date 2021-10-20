72728e9278ac157e90d61df13c9356a87180d526
Mellarin <alexleo2016@gmail.com>
2021-10-20 15:09:25 +0300

    commit captcha

--- views/registration.php
+++ views/registration.php
@@ -1,13 +1,15 @@
 <?php 
+session_start();
+$status = '';
 if(isset($_POST['submit']))
 {
-	if(preg_match('/^(?=.*[0-9])(?=.*[A-Z]).{8,}$/',$_POST['password']))
+	if(preg_match('^(?=.*[0-9])(?=.*[A-Z][a-z]).{10,}$',$_POST['password']))
 	{
 		
 	}else{
 		$error1="BAD PASSWORD";
 	}
-	if(preg_match('/^[a-z0-9-]+(\.[a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,10})$/',$_POST['email']))
+	if(preg_match('/^[a-z0-9-]{2,20}(\.[a-z0-9-]{2,20})*@[a-z0-9-]{3,10}+(\.[a-z0-9-]{3,5})*(\.[a-z]{2,10})$/',$_POST['email']))
 	{
 
 	}
@@ -15,7 +17,7 @@ if(isset($_POST['submit']))
 	{
 		$error2="BAD EMAIL";
 	}
-	if(preg_match('/^[A-Za-z][a-z0-9]{5,20}$/',$_POST['username']))
+	if(preg_match('^[A-Za-z][A-Za-z0-9]{3,20}$',$_POST['username']))
 	{
 
 	}
@@ -28,23 +30,16 @@ if(isset($_POST['submit']))
 		$error4 = "Passwords did not match!";
 	}
 	$accountData = json_decode(file_get_contents("./json/countries.json"), true);
-
 }
-?>
-<script>
-	fetch("countries.json")
-	.then(response => response.json())
-	.then(countries => {
-		var template = '';
-		for(var country of countries)
-		{
-			template+=<option value="${country.name}">${country.name}</option>
+if ( isset($_POST['captcha']) && ($_POST['captcha']!="") ){
+	if(strcasecmp($_SESSION['captcha'], $_POST['captcha']) != 0){
+	$status = "<p style='color:#FFFFFF; font-size:20px'><span style='background-color:#FF0000;'>Captcha is wrong,enter one more time</span></p>";
+	}else{
+	$status = "<p style='color:#FFFFFF; font-size:20px'><span style='background-color:#46ab4a;'>Greetings!Сaptcha is correct</span></p>";	
 		}
-		const l = document.getElementById('selector')
-		l.innerHTML=template;
 	}
-	)
-</script>
+?>
+
 <section class="register" id="register">
 <div class="container">
 		<form action="" method="POST" class="login-email">
@@ -83,14 +78,36 @@ if(isset($_POST['submit']))
 			</div>
 			<div class="input-group">
 				<a class="Chooose" style="font-size:2.5rem">Choose country</a>
-				<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-				<select id='select' style="font-size:2.5rem">
+                <script>
+	                fetch("countries.json")
+	                .then(response => response.json())
+	                .then(countries => {
+		            var template = '';
+		            for(var country of countries)
+		            {
+			          template+=`<option value="${country.name}">${country.name}</option>`
+		            }
+		           const l = document.getElementById('selector')
+		           l.innerHTML=template;
+	               }
+	               )
+             </script>
+				<select id='selector' style="font-size:2.5rem">
 					<option value="countries"></option>
 				</select>
 			</div>
 			<div class="input-group">
-				<button name="submit" class="btn">Register</button>
+		       <a><img src="captcha.php?rand=<?php echo rand(); ?>" id='captcha_image'></a>
+                <p style="color: #111;"><a style="text-decoration:none color: #111;" href='javascript: refreshCaptcha();'>Refresh</a></p>
+				<input type="captcha" placeholder="Captcha" required>		
 			</div>
+			<div class="input-group">
+				<br>
+				<br>
+				<button name="submit" class="btn">Register</button>
+			 </div>
+			    <br>
+				<br>
 			<p class="login-register-text">Have an account? <a href="index.php">Login Here</a>.</p>
 		</form>
 	</div>
@@ -100,6 +117,12 @@ if(isset($error))
 	echo $error;
 }
 ?>
+<script>
+function refreshCaptcha(){
+    var img = document.images['captcha_image'];
+    img.src = img.src.substring(0,img.src.lastIndexOf("?"))+"?rand="+Math.random()*1000;
+}
+</script>
 </section>
 
 
